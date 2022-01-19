189fa1d8884f7b664761671961cd677186f29ce6
aniketak47 <aniketkr20012019@gmail.com>
2022-01-19 20:39:04 +0530

    session 1

---
+++ Session 1/src/myApp.java
@@ -0,0 +1,11 @@
+package myApp;
+
+public class myApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("[MyApp] Our Goal is to develop solutions to save time :)");
+        System.out.println("[MyApp] Be Exceptional");
+	}
+
+}
